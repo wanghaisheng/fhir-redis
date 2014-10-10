357f5802bec274b0f036d4f0810a7a83c321c6a9
wanghaisheng <edwin_uestc@163.com>
2014-10-10 21:01:40 +0800

    getall and create success

--- redis-jedis-example/src/main/java/nv/fhir/example/FhirRedis.java
+++ redis-jedis-example/src/main/java/nv/fhir/example/FhirRedis.java
@@ -3,23 +3,15 @@ package nv.fhir.example;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.api.Bundle;
 import ca.uhn.fhir.model.api.IResource;
-import ca.uhn.fhir.model.dstu.resource.Device;
 import ca.uhn.fhir.model.dstu.resource.Patient;
-import ca.uhn.fhir.model.primitive.CodeDt;
-import ca.uhn.fhir.model.primitive.StringDt;
 import ca.uhn.fhir.parser.IParser;
-import ca.uhn.fhir.parser.JsonParser;
-import ca.uhn.fhir.parser.XmlParser;
-import com.alibaba.fastjson.JSON;
 import nv.fhir.utils.DateUtil;
 import nv.fhir.utils.RedisUtil;
 import nv.fhir.utils.StringUtil;
-import org.apache.commons.io.IOUtils;
+
 import redis.clients.jedis.Jedis;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.Iterator;
@@ -43,29 +35,17 @@ public class FhirRedis {
 //        获取redis实例
         Set<String> patitent_logicalID_set = redis.smembers("Patient:all");
         Iterator<String> testSet = patitent_logicalID_set.iterator();
-        List<IResource> patientALL = null;
-
+        List<IResource> patientALL =new ArrayList();
         while (testSet.hasNext()) {
             String patitent_logicalID = testSet.next();
             IParser parser = ctx.newJsonParser();
             String key = "Patient:" +patitent_logicalID+ ":content";
-            System.out.println("查到的逻辑标识符为："+patitent_logicalID);
-            System.out.println("组合成的redis中key为："+key);
             String data = redis.get(key);
-/*            try {
-                data =  IOUtils.toString( new ByteArrayInputStream(data.getBytes()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-            System.out.println("资源的字符串为："+ data);
             IParser p = ctx.newJsonParser();
-            // ourLog.info("Reading in message: {}", msg);
             Patient   res = p.parseResource(Patient.class,data);
-//            Patient res = parser.parseResource(Patient.class,redis.get(key));
+            System.out.println(res.getIdentifier().get(0).getUse().getValue()); // PRP1660
+            System.out.println(res.getIdentifier().get(0).getAssigner().getDisplay()); // PRP1660
            patientALL.add(res);
-
-
-//            patientALL.add(parser.parseResource(Patient.class, redis.get(key)));
         }
 
        return Bundle.withResources(patientALL,ctx,"localhost/fhir");
@@ -98,7 +78,16 @@ public class FhirRedis {
         Patient patient = parser.parseResource(Patient.class,list.get(0));
         return patient;
     }
+    public static final Patient GetSpecifiedVersionPatientWithLogicalID(String patitent_logicalID) {
 
+
+        // LRANGE key start
+        // stop返回列表key中指定区间内的元素，区间以偏移量start和stop指定。下标(index)参数start和stop都以0为底，也就是说，以0表示列表的第一个元素，以1表示列表的第二个元素，以此类推。你也可以使用负数下标，以-1表示列表的最后一个元素，-2表示列表的倒数第二个元素，以此类推。
+        List<String>   list = redis.lrange("Patient:patitent_logicalID:all", 0, -1);
+        IParser parser = ctx.newJsonParser();
+        Patient patient = parser.parseResource(Patient.class,list.get(0));
+        return patient;
+    }
     public static final void CreatePatientResource(){
         //hapi读取json文件中的数据
         IParser parser = ctx.newJsonParser();
@@ -106,10 +95,6 @@ public class FhirRedis {
 //        新增资源实例
         Patient patient = parser.parseResource(Patient.class, StringUtil.StringFromFile("/patient-example-a"));
         parser.setPrettyPrint(true);
-
-// The various datatype classes have accessors called getValue()
-        System.out.println(patient.getIdentifier().get(0).getUse().getValue()); // PRP1660
-        System.out.println(patient.getIdentifier().get(0).getAssigner().getDisplay()); // PRP1660
 
 //        生成GUID作为资源的logical ID
         String patitent_logicalID = GenerateGUID();
@@ -126,29 +111,12 @@ public class FhirRedis {
         String ContentAll_key = "Patient:" +patitent_logicalID+ ":all";
         String LastModifiedDate_key = "Patient:" +patitent_logicalID+ ":LastModifiedDate";
         String VersionId_key = "Patient:" +patitent_logicalID+ ":VersionId";
-        
-        
-//        patient.setId(patitent_logicalID);
-        String encoded = parser.encodeResourceToString(patient);
+       String  Search_Parameters_name_key = "Patient:name" +patient.getName()+ ":patitent_logicalID";;
+        String jsonencoded =parser.encodeResourceToString(patient);
 
-        try {
-            String msg = IOUtils.toString(XmlParser.class.getResourceAsStream("/examples-json/patient-example-a.json"));
+       redis.set(Content_key, jsonencoded);
 
-
-            redis.set(Content_key, msg);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-/*
-        System.out.println("key为"+Content_key);
-
-        System.out.println("存储的字符串为"+redis.get(Content_key));
-*/
-
-        redis.lpush(ContentAll_key, encoded);
+        redis.lpush(ContentAll_key, jsonencoded);
 
 //        为Metadata建立索引
         redis.set(LastModifiedDate_key, patitent_LastModifiedDate);
@@ -159,8 +127,8 @@ public class FhirRedis {
     }
     public static void main(String[] args) {
         FhirRedis test = new FhirRedis();
-        test.CreatePatientResource();
-        test.GetAllPatient();
+      test.CreatePatientResource();
+        FhirRedis.GetAllPatient();
 
     }
 }
